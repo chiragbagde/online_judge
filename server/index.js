712fe06447a96c78285c74517b0d9241230b6722
chiragbagde <chiragsbagde@gmail.com>
2023-11-01 22:31:22 +0530

    added Docker

--- server/index.js
+++ server/index.js
@@ -6,17 +6,19 @@ var jwt = require("jsonwebtoken");
 var cookieParser = require("cookie-parser");
 const { generateFile } = require("./generateFile");
 const { executeCpp } = require("./executeCpp");
+const cors = require("cors");
 
 const PORT = process.env.PORT || 5000;
 app = express();
 DBConnection();
 
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!!!");
+  res.send("<h1>Hello Everyone running inside from Docker</h1>");
 });
 
 app.post("/register", async (req, res) => {
@@ -98,9 +100,9 @@ app.post("/run", async (req, res) => {
   }
   const filePath = await generateFile(lang, code);
   const output = await executeCpp(filePath);
-  res.status(200).json({ filePath, output });
+  res.status(200).json({ output });
 });
 
-app.listen(8080, () => {
-  console.log("Server is running on port 8080!");
+app.listen(5000, () => {
+  console.log("Server is running on port 5000!");
 });
