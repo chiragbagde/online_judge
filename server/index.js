a9fe8d7626e2d01f7d0577ba709d1f7955ce1c08
chiragbagde <chiragsbagde@gmail.com>
2025-05-07 02:10:20 +0530

    cors test

--- server/index.js
+++ server/index.js
@@ -63,7 +63,12 @@ async function startServer() {
     clearRequireCacheForModels();
 
     app = express();
-    app.use(cors());
+    const corsOptions = {
+      origin: "http://localhost:3000",
+      credentials: true,
+    };
+
+    app.use(cors(corsOptions));
     app.use(express.json());
     app.use(cookieParser());
     app.use(express.urlencoded({ extended: true }));
